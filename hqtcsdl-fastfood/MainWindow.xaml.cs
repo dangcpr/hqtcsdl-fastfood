d7f7b6b12bbb0db3050d284c770df5dc46dacd2c
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2022-11-26 18:06:24 +0700

    VS 2017

--- hqtcsdl-fastfood/MainWindow.xaml.cs
+++ hqtcsdl-fastfood/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace hqtcsdl_fastfood
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
